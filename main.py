b5ccdfdf0af14c73dcb4f7e15913ab6c92f64c1a
Leoprieto <leoprietoc@gmail.com>
2019-04-04 20:28:41 -0500

    proyecto
    se agregan ejemplos de uso de condiciones en python

---
+++ main.py
@@ -0,0 +1,31 @@
+#x = input("Ingrese dato: ")
+#print(x)
+#y = input("Ingrese dato: ")
+#print(x + y)
+#x = int(input("Ingrese dato: "))
+#print(x)
+#y = int(input("Ingrese dato: "))
+#print(x + y)
+#a = True
+#b = False
+#print(a)
+""" a = 5
+if a<4:
+    print("hola")
+    print("mundo")
+elif a>5:
+    print("es menor que 5")
+else:
+    print("no es menor")
+
+a = 7
+while a==7:
+    print("estoy en el while")
+    a = int(input("ingrese el nuevo valor de a: "))
+    if a !=7:
+        break """
+
+a = 3
+b= 4
+if a ==3 and b==4:
+    print("es correcto")
